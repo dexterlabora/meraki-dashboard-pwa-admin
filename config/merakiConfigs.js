25b639fe5fd11b666790d5ac40629e7ab3ee12be
Cory Guynn <cguynn@gmail.com>
2018-09-11 16:06:07 +0200

    setting demo as read-only

--- config/merakiConfigs.js
+++ config/merakiConfigs.js
@@ -2,9 +2,8 @@
 
 // Note: the apiKey can be overwritten if the client provides its own headers.
 
-module.exports = 
-{
-    apiKey: '2f301bccd61b6c642d250cd3f76e5eb66ebd170f', // Meraki Sandbox Demo
-    //apiUrl: 'https://us-central1-meraki-dashboard-api.cloudfunctions.net/api' //'http://localhost:8085/api' //'https://n149.meraki.com/api/v0'
-    apiUrl: 'https://api.meraki.com/api/v0'
-}
+module.exports = {
+  apiKey: "6bec40cf957de430a6f1f2baa056b99a4fac9ea0", // Meraki Sandbox Demo - ReadOnly
+  //apiUrl: 'https://us-central1-meraki-dashboard-api.cloudfunctions.net/api' //'http://localhost:8085/api' //'https://n149.meraki.com/api/v0'
+  apiUrl: "https://api.meraki.com/api/v0"
+};
