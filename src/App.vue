22c443a7a42447bb32161552c3909e62354ca265
Cory Guynn <cguynn@gmail.com>
2018-09-13 22:27:06 +0200

    updated app name in title

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
     <v-toolbar :color="!adminMode ? 'success' : 'warning'">
       <v-toolbar-side-icon @click.native.stop="showNav = !showNav"></v-toolbar-side-icon>
       <v-spacer></v-spacer>
-      <v-toolbar-title class="white--text">Meraki Tools</v-toolbar-title>
+      <v-toolbar-title class="white--text">Meraki Vue Tools</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items >
         <settings></settings> 
